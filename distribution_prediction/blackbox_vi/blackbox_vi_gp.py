2ab91251977def3b8b4aaa3da518334694c0b6f6
benjaminleroy <bcl21@ic.ac.uk>
2022-03-10 15:29:25 +0000

    marg

--- distribution_prediction/blackbox_vi/blackbox_vi_gp.py
+++ distribution_prediction/blackbox_vi/blackbox_vi_gp.py
@@ -133,12 +133,9 @@ def expected_log_marginal_likelihood(mu: np.ndarray,
         print(epsilon[s])
         theta_s = theta_s[0]
 
-        #prior = 1 / np.sqrt((2 * np.pi)** 6 * np.linalg.det(sigma)) * np.exp(-0.5 * ((theta_s - mu) @ np.linalg.inv(sigma) @ (theta_s - mu).T)[0][0])
-        #log_prior=np.log(prior)
+
         log_prior=np.log(1/np.sqrt(2*np.pi)**6*np.exp(-0.5*np.linalg.norm(epsilon[s])**2))
-        #log_prior=0
-        #print(np.linalg.det(sigma))
-        #print(np.exp(-0.5 * ((theta_s - mu) @ np.linalg.inv(sigma) @ (theta_s - mu).T)[0][0]))
+
         print("log_prior")
         print(log_prior)
         #log_prior=np.log(1/np.sqrt(2*np.pi)**6*np.exp(-0.5*np.linalg.norm(theta_s)**2))
